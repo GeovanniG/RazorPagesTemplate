3dce210152ea3aa99f49818d4a049f09b7d0da41
Geovanni Gonzalez <32276348+GeovanniG@users.noreply.github.com>
2023-06-12 18:37:02 -0700

    Divide code into separate projects

---
+++ ResumeVault.Application/ConfigureServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ResumeVault.Application;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        _ = services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        return services;
+    }
+}
---
+++ ResumeVault.Auth/Infrastructure/AuthDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ResumeVault.Auth.Infrastructure;
+
+public class AuthDbContext : IdentityDbContext
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        _ = builder.Entity<IdentityUser>().ToTable("users");
+        _ = builder.Entity<IdentityRole>().ToTable("roles");
+        _ = builder.Entity<IdentityUserRole<string>>().ToTable("user_roles");
+        _ = builder.Entity<IdentityUserClaim<string>>().ToTable("user_claims");
+        _ = builder.Entity<IdentityUserLogin<string>>().ToTable("user_logins");
+        _ = builder.Entity<IdentityUserToken<string>>().ToTable("user_tokens");
+        _ = builder.Entity<IdentityRoleClaim<string>>().ToTable("role_claims");
+    }
+}
